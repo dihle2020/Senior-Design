978d6974acf5ea01e48d80d6910f67bf3d1541ee
Patrick <pseminatore@gmail.com>
2019-12-04 10:40:21 -0800

    morning of presentation commit!

--- controller_workingonfile.py
+++ controller_workingonfile.py
@@ -8,9 +8,6 @@ import tensorflow
 from tensorflow import keras
 import numpy as np
 from PIL import Image
-from playsound import playsound
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
 
 
  #Shutterspeed : 1/100
@@ -23,15 +20,15 @@ def main():
     loading_start = time.time()
 
     # Load the model
-    ballinframe_model = tensorflow.keras.models.load_model('blue_balls_2.h5', compile=False)
-    shotmade_model = tensorflow.keras.models.load_model('thomas_blue_model.h5', compile=False)
+    ballinframe_model = tensorflow.keras.models.load_model('keras_model_ballinframe.h5', compile=False)
+    shotmade_model = tensorflow.keras.models.load_model('thomas_model3.h5', compile=False)
     
     # Create Queue for resource (in this case Image) sharing
     q = Queue()
 
     # In PRODUCTION, add filename from UI as argument
     # Create Process for preprocessing of images
-    pipeline = Process(target=vpp.run_webcam, args=(q, ))
+    pipeline = Process(target=vpp.run_file, args=(q, 'missmakemake.mp4'))
 
     # start video preprocessing
     pipeline.start()
@@ -42,7 +39,7 @@ def main():
     new_images = []
     
     # Number of Frames to Skip
-    skip_ballinframe = 4
+    skip_ballinframe = 2
     
 
     # Set up flags for 2-Flag decision system
@@ -85,37 +82,34 @@ def main():
                 # Obtain Ball-In-Frame prediction
                 if current_frame % skip_ballinframe == 0:
                     ballinframe_prediction = ballinframe_model.predict(data)[0]
-                    #print(ballinframe_prediction)
+                    print(ballinframe_prediction)
                     if ballinframe_prediction[0] > 0.95 and counter == 0:
                         in_frame = True
                         attempted = True
                         finished = False
-                        counter = 45
+                        counter = 50
                         print("From controller ----------------------------------------------------------------------> Shot attempted")
-                    if ballinframe_prediction[1] > 0.5:
-                        in_frame = False
-                        finished = True
-                        if attempted:
-                            print("From Controller -------------------------------------------------------------------------------> Shot Missed")
-                            playsound('boo.mp3')
-                #print(counter)
+                if ballinframe_prediction[1] > 0.6:
+                    in_frame = False
+                    finished = True
+                    if attempted:
+                        print("From Controller -------------------------------------------------------------------------------> Shot Missed")
+                print(counter)
                 if counter > 0:
-                    #print(attempted, finished)
+                    print(attempted, finished)
                     if attempted and not finished:
                         shotmade_prediction = shotmade_model.predict(data)[0]
                         print(shotmade_prediction)
                         if shotmade_prediction[2] > 0.5 and not above_rim:
                             above_rim = True
                             pass
-                        if shotmade_prediction[0] > 0.1 and above_rim and not in_hoop:
+                        if shotmade_prediction[0] > 0.5 and above_rim and not in_hoop:
                             in_hoop = True
+                            pass
+                        if shotmade_prediction[1] > 0.5 and above_rim and in_hoop and not below_hoop:
+                            below_hoop = True
                             finished = True
                             print("From Controller ----------------------------------------------------------------------------------> Shot Made")
-                            playsound('Ding Sound Effect.mp3')
-                            pass
-                        if shotmade_prediction[1] > 0.1 and above_rim and in_hoop and not below_hoop:
-                            below_hoop = True
-                            
                 if not counter == 0:
                     counter -= 1
                 if finished:
